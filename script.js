a7167ba33644ee47b59badb74ab34f7d1e41ef3c
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2024-10-07 23:59:24 +0600

    Give squares appropriate width and height

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const container = document.querySelector('.container');
 
 const numSquaresPerSide = 16;
 const numSquares = numSquaresPerSide * numSquaresPerSide;
+const squareWidth = container.offsetWidth / numSquaresPerSide;
 
 for (let i = 0; i < numSquares; ++i) {
   const square = document.createElement('div');
   square.textContent = 'square';
   square.classList.add('square');
+  square.style.width = square.style.height = `${squareWidth}px`;
   container.appendChild(square);
 }
