44544e02cf441a02a2ebbc289e63283850ddb3f7
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2024-10-20 01:19:00 +0600

    Refactor the grid creation code

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ function blacken(event) {
     event.target.classList.add('black-bg');
 }
 
+function createNewGrid(numSquaresPerSide) {
+  const numSquares = numSquaresPerSide * numSquaresPerSide;
+  const squareWidth = container.offsetWidth / numSquaresPerSide;
+  const squares = [];
+
+  for (let i = 0; i < numSquares; ++i) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = square.style.height = `${squareWidth}px`;
+    squares.push(square);
+  }
+
+  container.replaceChildren(...squares);
+}
+
 const container = document.querySelector('.container');
 container.addEventListener('mouseover', blacken);
 
-const numSquaresPerSide = 16;
-const numSquares = numSquaresPerSide * numSquaresPerSide;
-const squareWidth = container.offsetWidth / numSquaresPerSide;
-
-for (let i = 0; i < numSquares; ++i) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  square.style.width = square.style.height = `${squareWidth}px`;
-  container.appendChild(square);
-}
+createNewGrid(16);
